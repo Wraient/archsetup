d27777665b434e5e9c448c609c795743505e546e
wraient <wraient.911@gmail.com>
2024-02-21 23:13:07 +0530

    Auto commit

---
+++ startup_git_sync.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# List of file paths and folder paths to copy
+paths=(
+    "/home/wraient/.bashrc"
+    "/home/wraient/.zshrc"
+    "/home/wraient/.xinitrc"
+    "/home/wraient/.ssh/known_hosts"
+    "/home/wraient/.ssh/config"
+    "/home/wraient/.bash_profile"
+    "/home/wraient/pix/wall"
+    # Add more file and folder paths as needed
+)
+
+# Copy files and folders to archsetup folder
+for path in "${paths[@]}"; do
+    if [ -f "$path" ]; then
+        # If it's a file, copy it directly
+        filename=$(basename "$path")
+        cp "$path" ~/.hidden/archsetup/"$filename" 2>/dev/null
+    elif [ -d "$path" ]; then
+        # If it's a folder, copy its contents recursively
+        dirname=$(basename "$path")
+        cp -r "$path" ~/.hidden/archsetup/"$dirname" 2>/dev/null
+    fi
+done
+
